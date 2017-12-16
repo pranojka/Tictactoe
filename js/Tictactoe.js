cc7133c26f1a54f4a50669f85156c2ec7eb5dab3
pranojka <jovana.pranic@gmail.com>
2017-12-16 14:11:15 -0800

    lglg

--- js/Tictactoe.js
+++ js/Tictactoe.js
@@ -1,7 +1,7 @@
 function drawBoard() {
     var cells = ['', '', '', '', '', '', '', '', ''];
     var table = document.getElementById("game");
-    indicator = 1;
+    var counter = 1;
     var i;
     var x;
 
@@ -9,28 +9,33 @@ function drawBoard() {
         var row = document.createElement("tr");
 
         for (var x = 0; x < 3; x++) {
-            var col = document.createElement("td");
+            var td = document.createElement("td");
             //col.innerHTML = cells;
-            row.appendChild(col);
+            td.id = counter;
+
+            row.appendChild(td);
         }
         table.appendChild(row);
         // col.indicator=indicator;
         // col.onclick=set;
 
 
+
     }
+
 }
 
 function refresh() {
+    var cells = ['', '', '', '', '', '', '', '', ''];
 
     location.reload();
 }
 
-function Play() {
-    var cells = ['', '', '', '', '', '', '', '', ''];
-    var i;
-    var playerone;
-    var playertwo;
-    var turn;
-    for ()
-}
+// function Play() {
+//     var cells = ['', '', '', '', '', '', '', '', ''];
+//     var i;
+//     var playerone;
+//     var playertwo;
+//     var turn;
+//     for ()
+// }
